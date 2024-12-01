1c202281d41de7693eda5a4c7b9dc6560ccb35dd
dotubutis <dotubutis@gmail.com>
2024-11-30 22:15:46 -0500

    stop at 38:09

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Header from './Header.svelte';
+	let name = $state('Donatas');
+
+	let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+
+	let full_name = $derived(name + ' ' + 'Tubutis');
+
+	function toggle() {
+		// Mutate the variable itself!
+		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
+	}
+</script>
+
+<!-- can also write as <Header {name}> -->
+<Header {name} fake_name={'brolis'} />
+
+<h2>{full_name}</h2>
+<input type="text" bind:value={name} />
+
+<p>The store is now {status}</p>
+<p></p>
+<button onclick={toggle}>Toggle Status</button>
+
+<!-- Could also do this shorthand if there is a named function onclick-->
+<!-- <button {onclick}>Toggle.... -->
+
+<!-- Can also do it inline! -->
+<button
+	onclick={() => {
+		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
+	}}>Toggle Status</button
+>
+
+<style>
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	// $props is a rune, new svelte 5 feature
+	let {
+		name,
+		fake_name
+	}: {
+		name: string;
+		fake_name?: string;
+	} = $props();
+</script>
+
+<h1>Welcome to the shop {name}!</h1>
+
+{fake_name}
+
+<style>
+</style>
